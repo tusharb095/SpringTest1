1c4e8edb5c7489db33ea3cc41509d2348cd3e468
tusharb095 <tusharbholane095@gmail.com>
2017-12-14 19:50:25 +0530

    code updated

--- src/main/java/go/app/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package go.app.repository;
-
-import go.app.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- src/main/java/go/app/repository/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package go.app.repository;
-
-import go.app.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {}
